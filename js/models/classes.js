b7419705383653c53f615cb5d57d27b6ef59f734
Zara El Alaoui <fat01471@tjx.com>
2022-08-09 11:49:56 -0400

    adding classes

---
+++ js/models/classes.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const { connection } = require('../connection');
+
+let classes = connection.define(
+  'classes',
+  {
+      class_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false
+      },
+      seats: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      semester: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }, 
+      class_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      instructor_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      room_id: DataTypes.INTEGER,
+  },
+  {
+    createdAt: false,
+    updatedAt: false,
+    underscored: true
+    
+  }
+);
+
+module.exports = classes;
